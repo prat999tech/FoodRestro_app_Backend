e52756b2c01e4cd8137feb8ef77eeaf1997a8451
pratik singh <prateeksingh26@outlook.com>
2025-05-21 18:06:57 +0530

    updated

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -52,4 +52,37 @@
 
 
  },{timestamps:true})
+ userschema.pre("save",async function(next){
+    if(!this.isModified("password"))
+        return next();
+     this.password= await bcrypt.hash(this.password,10);
+     next();
+ })
+ userschema.methods.isPasswordCorrect=async function(password){
+    return await bcrypt.compare(password,this.password);
+
+ }
+
+     userSchema.methods.generateAccessToken = function(){
+    return jwt.sign({
+         _id:this.id,
+         email:this.email, 
+         username:this.username,
+         fullName:this.fullname,
+    },
+process.env.ACCESS_TOKEN_SECRET,
+{
+    expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+})
+} 
+userSchema.methods.generateRefreshToken = function(){
+    return jwt.sign({
+         _id:this.id,
+         
+    },
+process.env.REFRESH_TOKEN_SECRET,
+{
+    expiresIn: process.env.REFRESH_TOKEN_EXPIRY
+})
+}
  export const User = mongoose.model('User',userschema);
